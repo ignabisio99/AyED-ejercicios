e3cfadd63f4004badf4c1f144f3ad82e7ee2672e
Ignacio Bisio <83171035+ignabisio99@users.noreply.github.com>
2022-01-07 14:58:51 -0300

    Create ejercicio1.cpp

---
+++ Guia 1/ejercicio1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{ 
+int numero1;
+    int numero2;
+
+    cout << "Ingrese un numero: " << endl;
+    cin >> numero1;
+    cout << "Ingrese otro numero: " << endl;
+    cin >> numero2;
+
+    cout << "La suma es: " << numero1 + numero2 << endl;
+    cout << "La resta es: " << numero1 - numero2 << endl;
+    cout << "La multiplicacion es: " << numero1 * numero2 << endl;
+     return 0;
+}
