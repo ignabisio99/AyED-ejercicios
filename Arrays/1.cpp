587bf65d8899a7c41638140604d46662e6a9712c
Ignacio Bisio <83171035+ignabisio99@users.noreply.github.com>
2022-01-10 14:47:10 -0300

    Create 1.cpp

---
+++ Arrays/1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+
+
+
+int main()
+{
+    int vec[10];
+
+    for(int i = 0; i <= 10; i++){
+    cout << "Ingrese un valor numerico: ";
+        cin >> vec[i];
+    }
+
+    cout << "El orden inverso de los valores ingresado es:" << endl;
+
+    int cont = 10;
+
+    while(cont >= 0){
+        cout << vec[cont - 1] << " ";
+        cont = cont - 1;
+    }
+
+
+    return 0;
+}
