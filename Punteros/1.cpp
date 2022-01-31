5a64d596a1d9faf6524b49d5e6f71e2cb2547a72
Ignacio Bisio <83171035+ignabisio99@users.noreply.github.com>
2022-01-31 17:45:29 -0300

    Update 1.cpp

--- Punteros/1.cpp
+++ Punteros/1.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+ 
+using namespace std;
 
+
+int main()
+{   
+    int vec[5];
+    
+    int *puntero = vec;
+
+    for(int i=0;i<5;i++)
+    {
+        cout << "Ingrese un numero a guardar en el vector:" << endl;
+        cin >> *(puntero+i);
+    }
+
+    cout << "Los numeros ingresados en el vector son:" << endl;
+
+    for(int i = 0;i<5;i++)
+    {
+        cout << *(puntero+i) << " ";
+    }
+
+
+
+    return 0;
+}
