1b3ac709aa6bb76b3adb0fc35ae34dd2525504b3
Ignacio Bisio <83171035+ignabisio99@users.noreply.github.com>
2022-02-17 18:28:52 -0300

    INCOMPLETO

---
+++ Finales/final25-07-2021.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+// PUNTO 1 - Declarar todas las estructuras de datos involucradas en la aplicación que se propone
+
+
+struct CobroAutomatico
+{
+    int horaDeRegistro;
+    char patente[7];
+    int tipoVehiculo;
+
+};
+
+int main()
+{
+    
+
+
+    return 0;
+}
