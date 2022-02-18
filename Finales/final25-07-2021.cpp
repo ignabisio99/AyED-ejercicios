b4cdb996715c2cf9a4062baf6f5280e628ac1970
Ignacio Bisio <83171035+ignabisio99@users.noreply.github.com>
2022-02-18 19:42:42 -0300

    ULTRA DUDOSO LA RESOLUCION

--- Finales/final25-07-2021.cpp
+++ Finales/final25-07-2021.cpp
@@ -7,16 +7,84 @@ using namespace std;
 
 
 struct CobroAutomatico
-{
+{   
+    char nombreCabina[15];
     int horaDeRegistro;
     char patente[7];
     int tipoVehiculo;
 
 };
 
+struct CobroManual
+{
+    char nombreCabina[15];
+    int horaDeRegistro;
+    char patente[7];
+    int tipoVehiculo;
+};
+
+struct CobroMixto
+{
+    char nombreCabina[15];
+    int horaDeRegistro;
+    char patente[7];
+    int tipoVehiculo;
+    char modalidadDePago; // M (manual), P (automatico)
+
+};
+
+struct InfoArchivo
+{
+    int horaDeRegistro;
+    char patente[7];
+    int tipoVehiculo;
+    char modalidadDePago;
+};
+
+// PUNTO 3 - Codificar o diagramar la función void aparearArchivos(lista de parametros) que recibe por parámetro 
+//              los archivos dato asi como el que resultara del apareo de los mismos. (5 puntos)
+
+void aparearArchivos(FILE *telePase1,FILE *telePase2,FILE *coborManual1,FILE *cobroManual2, FILE *pagoMixto1, FILE *pagoMixto2,FILE *archivoResultante)
+{
+    CobroManual datoManualLeido;
+    CobroAutomatico datoAutomaticoLeido;
+    CobroMixto datoMixtoLeido;
+    InfoArchivo datoAInsertar;
+
+    // TOMO COMO SI TODOS LOS ARCHIVOS YA FUERON ABIERTOS
+
+    fread(&datoManualLeido,sizeof(CobroManual),1,telePase1);
+    while(!feof(telePase1))
+    {
+        datoAInsertar.horaDeRegistro = datoManualLeido.horaDeRegistro;
+        datoAInsertar.modalidadDePago = 'M';
+        strcpy(datoAInsertar.patente,datoManualLeido.patente);
+        datoAInsertar.tipoVehiculo = datoManualLeido.tipoVehiculo;
+
+        fwrite(&datoAInsertar,sizeof(InfoArchivo),1,archivoResultante); 
+
+        fread(&datoManualLeido,sizeof(CobroManual),1,telePase1);
+    }
+
+    // SE REPETIRIA ESTO POR CADA ARCHIVO
+
+
+    return;
+};
+
+
 int main()
 {
+    // PUNTO 2 - Declare y ponga a disposición los flujos de datos involucrados
     
+    FILE *telePase1; // Archivos por cada uno de las cabinas, cada vez que pasa un auto el archivo se va cargando
+    FILE *telePase2;
+    FILE *cobroManual1;
+    FILE *cobroManual2;
+    FILE *pagoMixto1;
+    FILE *pagoMixto2;
+
+    FILE *archivoResultante;
 
 
     return 0;
